17dea18f158676ee18ea01f32cb3a9549b8ba74a
Samuel Sml <veramirandasamuel6@gmail.com>
2024-04-04 09:08:43 -0500

    Student module done

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Student, StudentSchema } from './student.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Student.name,
+        schema: StudentSchema,
+      },
+    ]),
+  ],
   controllers: [StudentsController],
-  providers: [StudentsService]
+  providers: [StudentsService],
 })
 export class StudentsModule {}
