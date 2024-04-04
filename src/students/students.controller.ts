7e46edfca327007553890c0fb3f028cbc97f79c7
Samuel Sml <veramirandasamuel6@gmail.com>
2024-04-04 09:38:05 -0500

    Mehotds CRUD imported

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { StudentsService } from './students.service';
 
 @Controller('students')
-export class StudentsController {}
+export class StudentsController {
+  constructor(private readonly studentService: StudentsService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.studentService.findAll();
+  }
+
+  @Post('create')
+  createStudent(@Body() body) {
+    return this.studentService.create(body);
+  }
+
+  @Put('update/:id')
+  updateStudent(@Param('id') id: number, @Body() body) {
+    return this.studentService.update(id, body);
+  }
+
+  @Delete('delete/:id')
+  deleteStudent(@Param('id') id: number) {
+    return this.studentService.deleteStudent(id);
+  }
+}
