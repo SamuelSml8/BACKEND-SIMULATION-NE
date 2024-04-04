ac9f7b5d7eb504b9ac1426af3e3328ee3f1b9c9f
Samuel Sml <veramirandasamuel6@gmail.com>
2024-04-04 09:05:45 -0500

    Student entity done and db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentsModule } from './students/students.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://samuel8:t4ImxkPNMXAhIrLt@users-nodejs.eyxgrp5.mongodb.net/',
+      { dbName: 'students_simulation' },
+    ),
+    StudentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
