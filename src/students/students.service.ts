580e5044e71971b45b521fbdc21e8ae8c3b1f196
Samuel Sml <veramirandasamuel6@gmail.com>
2024-04-05 08:45:58 -0500

    try catch added to methods

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -10,103 +10,139 @@ export class StudentsService {
   ) {}
 
   async findAll() {
-    const students = await this.studentModel.find().exec();
-    return {
-      ok: true,
-      message: 'All students',
-      data: students,
-    };
+    try {
+      const students = await this.studentModel.find().exec();
+      return {
+        ok: true,
+        message: 'All students',
+        data: students,
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        ok: false,
+        message: 'Error Internal Server',
+        data: null,
+      };
+    }
   }
 
   async create(
     @Body() body,
   ): Promise<{ ok: boolean; message: string; data: Student }> {
-    const studantData = {
-      name: body.name,
-      identification: body.identification,
-      age: body.age,
-    };
+    try {
+      const studantData = {
+        name: body.name,
+        identification: body.identification,
+        age: body.age,
+      };
 
-    if (
-      studantData.name.length == 0 ||
-      studantData.identification.length == 0 ||
-      studantData.age.length == 0
-    ) {
+      if (
+        studantData.name.length == 0 ||
+        studantData.identification.length == 0 ||
+        studantData.age.length == 0
+      ) {
+        return {
+          ok: false,
+          message: 'All fields required',
+          data: null,
+        };
+      }
+
+      const student = new this.studentModel(studantData);
+      const studentSaved = await student.save();
+      return { ok: true, message: 'Studen created', data: studentSaved };
+    } catch (error) {
+      console.log(error);
       return {
         ok: false,
-        message: 'All fields required',
+        message: 'Error Internal Server',
         data: null,
       };
     }
-
-    const student = new this.studentModel(studantData);
-    const studentSaved = await student.save();
-    return { ok: true, message: 'Studen created', data: studentSaved };
   }
 
   async update(
     @Param('id') id: number,
     @Body() body,
   ): Promise<{ ok: boolean; message: string; data: Student }> {
-    const studentFound = await this.studentModel.findOne({
-      identification: id.toString(),
-    });
+    try {
+      const studentFound = await this.studentModel.findOne({
+        identification: id.toString(),
+      });
 
-    if (!studentFound) {
+      if (!studentFound) {
+        return {
+          ok: false,
+          message: 'Student not found',
+          data: null,
+        };
+      }
+
+      const newStudent = {
+        name: body.name,
+        identification: body.identification,
+        age: body.age,
+      };
+
+      if (
+        newStudent.name.length == 0 ||
+        newStudent.identification.length == 0 ||
+        newStudent.age.length == 0
+      ) {
+        return {
+          ok: false,
+          message: 'All fields required',
+          data: null,
+        };
+      }
+
+      await studentFound.updateOne(newStudent);
+      const studentUpdated = await this.studentModel.findOne({
+        identification: id.toString(),
+      });
+      return {
+        ok: true,
+        message: 'Student updated succesfully',
+        data: studentUpdated,
+      };
+    } catch (error) {
+      console.log(error);
       return {
         ok: false,
-        message: 'Student not found',
+        message: 'Error Internal Server',
         data: null,
       };
     }
-
-    const newStudent = {
-      name: body.name,
-      identification: body.identification,
-      age: body.age,
-    };
-
-    if (
-      newStudent.name.length == 0 ||
-      newStudent.identification.length == 0 ||
-      newStudent.age.length == 0
-    ) {
-      return {
-        ok: false,
-        message: 'All fields required',
-        data: null,
-      };
-    }
-
-    await studentFound.updateOne(newStudent);
-    const studentUpdated = await this.studentModel.findOne({
-      identification: id.toString(),
-    });
-    return {
-      ok: true,
-      message: 'Student updated succesfully',
-      data: studentUpdated,
-    };
   }
 
   async deleteStudent(@Param('id') id: number) {
-    const studentFound = await this.studentModel.findOne({
-      identification: id.toString(),
-    });
+    try {
+      const studentFound = await this.studentModel.findOne({
+        identification: id.toString(),
+      });
 
-    if (!studentFound) {
+      if (!studentFound) {
+        return {
+          ok: false,
+          message: 'Student not found',
+          data: null,
+        };
+      }
+
+      await studentFound.deleteOne();
+      return {
+        ok: true,
+        message: 'Student deleted succesfully',
+        data: studentFound,
+      };
+    } catch (error) {
+      console.log(error);
       return {
         ok: false,
-        message: 'Student not found',
+        message: 'Error Internal Server',
         data: null,
       };
     }
-
-    await studentFound.deleteOne();
-    return {
-      ok: true,
-      message: 'Student deleted succesfully',
-      data: studentFound,
-    };
   }
 }
